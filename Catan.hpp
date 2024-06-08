dcecbbb38a945a4f3dc85d4e44bd0ed13ac75d95
Uriel Shapiro <uriel1999@gmail.com>
2024-06-08 21:22:36 +0300

    Made GameConsts namespace

--- Catan.hpp
+++ Catan.hpp
@@ -1,23 +1,41 @@
+#pragma once
 #include "Player.hpp"
+#include "Board.hpp"
+
 #define MAX_NUM_OF_PLAYERS 3
 #define POINTS_TO_WIN 10
 
-enum ResourceCard
+namespace GameConsts
 {
-    Brick,
-    Wood,
-    Ore,
-    Grain,
-    Wool
-};
+    enum ResourceCard
+    {
+        Brick,
+        Wood,
+        Ore,
+        Grain,
+        Wool
+    };
+    enum MapValues
+    {
+        FOREST,
+        HILL,
+        MOUNTAIN,
+        FIELD,
+        PASTURES,
+        SEA,
+        DESERT
+    };
+}
 
 namespace ariel
 {
     class Catan
     {
-        private:
-            
-        public:
+    private:
+        Player players[MAX_NUM_OF_PLAYERS];
+        Board &board;
 
+    public:
+        Catan();
     }
 }
