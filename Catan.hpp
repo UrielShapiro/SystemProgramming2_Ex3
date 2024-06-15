e8c6d34b8607924c4bc0b2ed28267fb1b45f9578
Uriel Shapiro <uriel1999@gmail.com>
2024-06-15 21:01:14 +0300

    Changed rollDice function to return the sum of the dices

--- Catan.hpp
+++ Catan.hpp
@@ -20,7 +20,7 @@ namespace ariel
         Catan(ariel::Player &p1, ariel::Player &p2, ariel::Player &p3);
         Board *getBoard();
         void StartingGame(std::vector<size_t> &e_placement, std::vector<size_t> &v_placement);
-        void rollDice();
+        int rollDice();
         bool check_winner();
         bool check_largest_army();
         GameConsts::DevelopmentCard buyDevelopmentCard(Player &p);
